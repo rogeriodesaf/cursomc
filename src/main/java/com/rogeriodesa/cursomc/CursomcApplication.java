c316f82703907ccd768ffd7239e975146e2c9d10
Rogério de Sá <rogeriodesaf@gmail.com>
2020-03-12 16:13:00 -0300

    Criando operação de instanciação

--- src/main/java/com/rogeriodesa/cursomc/CursomcApplication.java
+++ src/main/java/com/rogeriodesa/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.rogeriodesa.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.rogeriodesa.cursomc.domain.Categoria;
+import com.rogeriodesa.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {  //	O commandLineRunner 
+	//TODO permite criar tabelas no banco de dados
+
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository; // chamando a classe que grava no banco
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); //isso vai salvar as categorias no banco
+	}
+
+	
 }
